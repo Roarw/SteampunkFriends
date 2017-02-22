088188e48f1c9f63c178507f7846a7b6db345f4b
UserNameIsAlreadyTaken1 <holsteinkruse@gmail.com>
2017-02-22 22:03:00 +0100

    Things shouldnt collide with themselves

--- SteampunkFriends/CPPGameWorld/Wall.cpp
+++ SteampunkFriends/CPPGameWorld/Wall.cpp
@@ -15,5 +15,5 @@ Wall::~Wall()
 
 void Wall::OnCollisionEnter(GameObject * other)
 {
-	gameObject->GetGameWorld()->DeleteObjectNext(other);
+	//gameObject->GetGameWorld()->DeleteObjectNext(other);
 }
