7484e04ff8eccf1aae9cbc030a9f3ad839674e95
UserNameIsAlreadyTaken1 <holsteinkruse@gmail.com>
2017-02-22 23:17:54 +0100

    Tried changing null check

--- SteampunkFriends/CPPGameWorld/Collider.cpp
+++ SteampunkFriends/CPPGameWorld/Collider.cpp
@@ -35,7 +35,7 @@ void Collider::CheckCollision()
 	// Loop through collisionboxes 
 	for (Collider * c : colliders)
 	{
-		if(c != nullptr && c != this)
+		if(c != this)
 		{ 
 			// Add simple collisioncheck before this if needed for performance 
 			if(c->Enabled)
@@ -66,7 +66,7 @@ void Collider::CheckCollision()
 
 	for (it = collisions.begin(); it != collisions.end(); ++it)
 	{
-		if (*it == nullptr)
+		if (*it == nullptr || *it == NULL)
 		{
 			toRemove.push(it);
 		}
